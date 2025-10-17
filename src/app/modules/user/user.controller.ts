ba12f071439eb81f2b4fd312707db75977aabafb
aticcmahbub <aticcmahbub@gmail.com>
2025-10-17 15:27:53 +0600

    utils: create catch async function for reducing try-catch repetition

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,19 +1,15 @@
-import {NextFunction, Request, Response} from 'express';
+import {Request, Response} from 'express';
 import {UserService} from './user.service';
+import {catchAsync} from '../../utils/catchAsync';
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const user = await UserService.createUser(req.body);
+const createUser = catchAsync(async (req: Request, res: Response) => {
+    const user = await UserService.createUser(req.body);
 
-        res.status(201).json({
-            message: 'User created successfully',
-            user,
-            success: true,
-        });
-    } catch (error) {
-        console.log(error);
-        next(error);
-    }
-};
+    res.status(201).json({
+        message: 'User created successfully',
+        user,
+        success: true,
+    });
+});
 
 export const UserController = {createUser};
